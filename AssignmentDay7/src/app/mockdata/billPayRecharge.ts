ed54a393d88d65e7f336d3542c5918c253eb19d0
Swati Nagpal <nagpalswati93@gmail.com>
2020-04-09 20:33:25 +0530

    Refactor

---
+++ AssignmentDay7/src/app/mockdata/billPayRecharge.ts
@@ -0,0 +1,10 @@
+export const BILL_PAYMENT_AND_RECHARGE_OPTIONS = [
+    "Electricity Bills",
+    "Telephone and Mobile Bills",
+    "Prepaid DTH/ Mobile Connection/ Data Card Recharge",
+    "Gas Bills",
+    "Mutual Funds.",
+    "Insurance Premiums.",
+    "Subscriptions.",
+    "Contribution to Charities"
+];
